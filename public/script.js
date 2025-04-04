ef1dbc0f5c71fe4c9ae437c28d8a630e67e95f7d
CompSciHigh24 <csh-github@compscihigh.org>
2025-04-04 09:57:41 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,3 +1,3 @@
 // Create and call a function that tests your update route handler [2 pts]
 // Make sure that the function takes the name of the potion as a parameter
-
+// There are already potion's with the label polyjuice or amortentia
