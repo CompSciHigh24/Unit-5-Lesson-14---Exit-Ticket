819b9b10d1f55a1f63d0c7b1fbce9367227a0cd8
CompSciHigh24 <csh-github@compscihigh.org>
2025-04-04 09:57:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ const potionSchema = new mongoose.Schema({
 
 const Potion = mongoose.model("Potion", potionSchema, "Potions")
 
-// Create a dynamic route handler that updates a specific potion's color based on it's name [2 pts]
+// Create a dynamic route handler that updates a specific potion's color based on its label [2 pts]
 // e.g. /update/polyjuice will update the color of the polyjuice potion to pearly white
 // e.g. /update/amortentia will update the color of the Amortentia potion to golden purple
-
 
